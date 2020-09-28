28bdc04902dbc087bdcfdf9b0adae608b87636e2
Alexander Hansen <alexander@helltann.no>
2020-09-29 00:05:37 +0200

    Made it docs instead of dist

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,11 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'docs')
     },
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'
+        contentBase: './docs'
     },
     plugins: [
         new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
